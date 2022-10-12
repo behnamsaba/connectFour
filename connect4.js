ff01e44428b8aef78e891f354cde741c7a1a5b56
ben <behnams71@gmail.com>
2022-10-12 11:53:25 -0700

    comments were added with winning conditons

--- connect4.js
+++ connect4.js
@@ -3,6 +3,11 @@ const HEIGHT = 6;
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
+
+/** makeBoard: create in-JS board structure:
+ *   board = array of rows, each row is array of cells  (board[y][x])
+ */
+
 function makeBoard() {
   for (let y = 0; y < HEIGHT; y++) {
     board.push(Array.from({ length: WIDTH }));
@@ -69,30 +74,19 @@ function handleClick(event){
     return endGame('Tie!');
   }
 
-  ////////////////////////////////////
-
-
-
-
   //switch player
   //my way
-  if(currPlayer === 1){
-    return currPlayer = 2;
-  }else if(currPlayer === 2){
-    return currPlayer = 1;
-  }
+  // if(currPlayer === 1){
+  //   return currPlayer = 2;
+  // }else if(currPlayer === 2){
+  //   return currPlayer = 1;
+  // }
 
-  //// switch players - better way with Conditional (ternary) operator.
-  // currPlayer = currPlayer === 1 ? 2 : 1;
+  // switch players - better way with Conditional (ternary) operator.
+  currPlayer = currPlayer === 1 ? 2 : 1;
 
 
 }
-
-
-
-
-
-
 
 function makeHtmlBoard(){
   const board = document.getElementById("board");
@@ -121,13 +115,7 @@ function makeHtmlBoard(){
     board.append(row);
   }
 
-
-
 }
-
-
-/// check for win according to sb code.//////////////////////////////////////////////////////////////////////
-
 
 function checkForWin() {
   function _win(cells) {
@@ -135,6 +123,7 @@ function checkForWin() {
     //  - cells: list of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
 
+    //after creating 4 cells in a row, we should check if all pieces belong to same player.
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
@@ -142,18 +131,22 @@ function checkForWin() {
         x >= 0 &&
         x < WIDTH &&
         board[y][x] === currPlayer
-    );
+    ); 
+
+
   }
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
+  //inorder to compare wining condition, we have to build 4 pieces and put in into array with nested loop 
+  //it looks like two-dimensional array as a matrix that has rows and columns
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      // find winner (only checking each win-possibility as needed) and if one of the condition met it return true to break the loop.
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
